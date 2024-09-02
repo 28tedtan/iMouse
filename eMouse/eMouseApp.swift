741b2f9653c2ab8ac6b34aed443097317a11bcbf
28tedtan <28tedtan@gmail.com>
2024-09-02 16:33:36 +0800

    Initial Commit

---
+++ eMouse/eMouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  eMouseApp.swift
+//  eMouse
+//
+//  Created by Ted Tan on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct eMouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
